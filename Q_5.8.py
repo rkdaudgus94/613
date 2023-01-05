0995bf12951c1a9238df2f3a8d166901d0a329e6
KIM2C1 <siwon9572@gmail.com>
2023-01-05 16:10:52 +0900

    Update Q_5.8.py

--- Q_5.8.py
+++ Q_5.8.py
@@ -1,7 +1,19 @@
 import copy
 x = list(map(list,input("정수를 입력하시오: ")))
 y = copy.deepcopy(x)
+count = 0
 
 x.reverse()
 print(x[:])
 print(y[:])
+print(type(len(x)))
+
+for i in range(len(x)):
+    if(x[i] == y[i]):
+        count += 1
+
+
+if(count == len(x)):
+    print("o")
+else:
+    print("x")
