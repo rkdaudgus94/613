1002c183b57e299a118821b9bfa752e99ed5279f
KIM2C1 <siwon9572@gmail.com>
2023-01-09 01:33:43 +0900

    Create Q_7.9.py

---
+++ Q_7.9.py
@@ -0,0 +1,18 @@
+x = list(input("문자열A: "))
+y = list(input("문자열B: "))
+
+print(x)
+print(y)
+
+for i in range(len(x)-1,-1,-1):
+    if x[i] == y[0]:
+        del y [0]
+    else:
+        break
+
+
+def listJoin(z):
+    result = ''.join(i for i in z)
+    return result
+
+print(listJoin(x)+listJoin(y))
