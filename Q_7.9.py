951902b3da4d2eaaaa624f4b82e275cb63acb00c
KIM2C1 <siwon9572@gmail.com>
2023-01-09 15:32:14 +0900

    Update Q_7.9.py

--- Q_7.9.py
+++ Q_7.9.py
@@ -1,18 +1,24 @@
 x = list(input("문자열A: "))
 y = list(input("문자열B: "))
+overlap_count = 0
 
 print(x)
 print(y)
 
-for i in range(len(x)-1,-1,-1):
-    if x[i] == y[0]:
-        del y [0]
-    else:
-        break
+#overlap counting
+for i in range(len(x)):
+    if y[:i+1] == x[-(1+i):]:
+        overlap_count = i+1
 
+#del y[]
+del y[:overlap_count]
 
+#join x and y
 def listJoin(index):
     result = ''.join(i for i in index)
     return result
 
+#result
+print("overlap 갯수:",overlap_count)
 print(listJoin(x)+listJoin(y))
+
